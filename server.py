d6e8c60636eb7b365d58df69ae2e7c58c97c2d16
ChelseaVPutri <chelseavp2706@gmail.com>
2025-06-22 01:04:55 +0700

    ga tau yah bener apa ngga

--- server.py
+++ server.py
@@ -1,6 +1,39 @@
 import socket
 import threading
 
+active_clients = []  # list of connected users
+
+# send new message to all clients that are connected to the server
+def sendMsgToAll(message):
+    for user in active_clients:
+        sendMsg(user[1], message)
+
+def sendMsg(client, message):
+    client.sendall(message.encode('utf-8'))
+
+# function to listen for upcoming messages from clients
+def listenMsg(client, username):
+    while True:
+        message = client.recv(1024).decode('utf-8')
+        if message != '':
+            final_msg = username + ": " + message
+            sendMsgToAll(message)
+        else:
+            print(f"Message from {username} is empty!")
+
+def clientHandler(client, address):
+    # server listen for client message that will contain the username
+    while True:
+        username = client.recv(1024).decode('utf-8')
+        if username != '':
+            active_clients.append((username, client))
+            break
+        else:
+            print("Username is empty!")
+    
+    threading.Thread(target=listenMsg, args=(client, username)).start()
+
+
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
@@ -11,17 +44,9 @@ except:
 
 server.listen()
 
-client, addr = server.accept()
+# server keep listening to client connections
+while True:
+    client, address = server.accept()
+    print(f"Successfully connected to client at {address[0]} {address[1]}")
 
-done = False
-
-while not done:
-    msg = client.recv(1024).decode('utf-8')
-    if msg == 'quit':
-        done = True
-    else:
-        print(msg)
-    client.send(input("Message: ").encode('utf-8'))
-
-client.close
-server.close
+    threading.Thread(target=clientHandler, args=(client, )).start()
